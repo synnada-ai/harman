fd1c1010b5a2b2c1cd01eadf2f094159ef63cc61
mehmetozsoy1 <146070048+mehmetozsoy1@users.noreply.github.com>
2024-11-12 11:15:31 +0300

    Update assign_reviewers.py

--- .github/scripts/assign_reviewers.py
+++ .github/scripts/assign_reviewers.py
@@ -3,7 +3,8 @@ import yaml #type[ignore]
 import subprocess
 
 # Load labels from GitHub Action input
-labels = sys.argv[1].split(',')
+labels = sys.argv[2].split(',')
+number = int(sys.argv[1])
 
 # Load the label-user mapping from the YAML file
 with open('.github/label_to_devs.yml', 'r') as file:
@@ -16,7 +17,7 @@ for label in labels:
     if label in label_user_mapping:
         reviewers.update(label_user_mapping[label])
     if reviewers:
-        prefix_arg = "gh pr edit --add-assignee "
+        prefix_arg = f"gh pr edit {number} --add-assignee "
         reviewers_arg = "".join(f"{reviewer} " for reviewer in reviewers)
         arg = prefix_arg + reviewers_arg
         subprocess.run(arg, shell=True)
