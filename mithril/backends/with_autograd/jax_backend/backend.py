b0054caa1e2fe82bf4b416af00e49c477f73f159
mehmetozsoy1 <mehmet.ozsoy@synnada.ai>
2024-11-12 11:34:23 +0300

    some changes are made

--- mithril/backends/with_autograd/jax_backend/backend.py
+++ mithril/backends/with_autograd/jax_backend/backend.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+print("JaxBackend")
 import math
 import os
 from collections.abc import Callable, Sequence
